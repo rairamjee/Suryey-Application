a712341148ccc9296731843cbeaaebefc7ebe1c5
Vikash Kumar <vikashkumar@jmangroup.com>
2024-08-07 22:57:50 +0530

    feat: kuch nahi hua

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,12 +1,20 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
+const BASE_STRING = 'http://localhost:3000/api'
 
 // This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
+export  function middleware(request: NextRequest) {
+    const user = localStorage.getItem('user');
+    console.log(user)
+    return null;
+    return new URL('/login', BASE_STRING)
+    if (user && request.nextUrl.pathname === '/login') {
+        return new URL('/dashboard/sidebar', BASE_STRING)
+    }
     return null;
 }
 
 // See "Matching Paths" below to learn more
-export const config = {
-    matcher: '/about/:path*',
-}
+// export const config = {
+//     matcher: '/about/:path*',
+// }
